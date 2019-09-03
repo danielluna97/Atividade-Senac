a68777bb1cc07c9689200a7e8d1b2fd51ba68f46
Sala 302B <sala302b@ceines.com.br>
2019-09-03 19:53:45 -0300

    atividade2

---
+++ Atividade.java/src/Atividade2.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sala302b
+ */
+public class Atividade2 {
+    public static void main(String[] args) {
+        
+    
+    
+    double lado1 = 5;
+    double lado2 = 15;
+    double mariola = misturaGoiba(lado1, lado2);
+    }
+    private static double misturaGoiba(double l1,double l2){
+        double r  = (l1 * l2) + (l1 * 2 + l2 * 2);
+        
+        
+        
+        return r;
+        
+        
+    }
+    
+   
+    }
+    
+    
+    
+            
+    
+    
+
